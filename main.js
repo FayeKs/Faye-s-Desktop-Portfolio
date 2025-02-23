588bddeba9dd334efe86a67bddf814ac993a510f
Faye Bishop <fayebishop44@gmail.com>
2025-02-23 13:15:06 -0600

    add taskbar section and show/hide icon function

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-// open and close button functions //
+// open and close button functions, taskbar icons funtion //
 
 const compModal = document.getElementById("compModal");
 const compBtn = document.getElementById("compBtn");
 const closeBtn = document.getElementById("closeBtn")
+const compTaskbar = document.getElementById("compTaskbar");
 
-// open computer modal button //
+// open computer modal button, show taskbar icon //
 compBtn.onclick = function() {
     compModal.style.display = "block";
+    compTaskbar.style.display = "block";
 }
-//close computer modal button//
+//close computer modal button, hide taskbar icon//
 closeBtn.onclick = function() {
-    compModal.style.display = "none";
+    compModal.style.display = "none"; 
+    compTaskbar.style.display = "none";
 }
+
+
+
+
