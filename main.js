5a7b0b8590578feb04e98d185fc5edcb5c17d382
Faye Bishop <fayebishop44@gmail.com>
2025-02-23 19:28:33 -0600

    add cpu usage widget function

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-// open and close button functions, taskbar icons funtion //
+// open, close, minimize button functions, taskbar icons funtion //
+
+const closeBtn = document.getElementById("closeBtn")
+const minBtn = document.getElementById("minBtn")
 
 const compModal = document.getElementById("compModal");
 const compBtn = document.getElementById("compBtn");
-const closeBtn = document.getElementById("closeBtn")
 const compTaskbar = document.getElementById("compTaskbar");
+
 
 // open computer modal button, show taskbar icon //
 compBtn.onclick = function() {
@@ -16,6 +19,25 @@ closeBtn.onclick = function() {
     compTaskbar.style.display = "none";
 }
 
+//minimize computer modal button funtion//
+minBtn.onclick = function(){
+    compModal.style.display = "none"
+}
+
+//open modal from taskbar icon function//
+compTaskbar.onclick = function(){
+    compModal.style.display = "block";
+}
 
 
 
+
+// CPU usage widget function //
+
+
+function updateCPUUsage(){
+    const cpuUsage = (Math.random() * (5- 0) + 0).toFixed(1); //  radnom number between 5% and 10%//
+    document.getElementById("cpuWidget").textContent = `CPU: ${cpuUsage}%;`
+}
+
+setInterval(updateCPUUsage, 1000); // updates every second //
